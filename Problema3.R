16bc11366f51fe569a6e2614dc792835a03a47cf
pau.santanach <pau.santanach@auleseebe.upc.edu>
2024-09-19 11:55:28 +0200

    pbm 3

--- Problema3.R
+++ Problema3.R
@@ -1,1 +1,11 @@
 #Codigo para problema 3
+x <- c(8, 5, 2, -8, 9, 5, 2, -3, 1, -1, 4, -4, 9, 3, -9, 7, 0, -7, 8, -4, 1, 7, -6, 4, 5, -9, -2, -8, 5, -5)
+
+sum(x) #fem la suma de tots els seus elements
+
+#per a selecionar una posició d'un vector fem x["posició"]
+x[3] #la pirmera posició és l'1 , no com al python
+
+
+
+#acabar a casa
