7aa9209e950ba0221ae46c3af91979447cbdbf48
pau.santanach <pau.santanach@auleseebe.upc.edu>
2024-09-19 11:54:51 +0200

    prblm 2 fet

--- Problema2.R
+++ Problema2.R
@@ -1,1 +1,13 @@
 #Codigo para problema 2
+
+ls()#listar todas las variables hasta el momento
+rm(list=ls()) #amb això borrem totes les dades que havia guardat abans
+mis_dades <- mtcars
+
+data = mtcars
+data
+hist(data$mpg) #aquesta linia seria ja la resposta al problema 2
+
+pie (data$mpg)
+pie(table(data$cyl))
+#això ultim son coses extres que ha fet
